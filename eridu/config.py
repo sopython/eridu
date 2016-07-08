c1e5488105240e9a5d87178360bbee592fca75db
Pizzey, Keiron (UK Gloucester) <keiron.pizzey@baesystems.com>
2016-07-08 12:02:42 +0100

    Added basic eridu logic.

---
+++ eridu/config.py
@@ -0,0 +1,21 @@
+import os
+
+
+ACCESS_KEY = os.environ.get('STACKEXCHANGE_REQUESTS_KEY')
+SITE = os.environ.get('ERIDU_SITE', 'stackoverflow')
+
+POST_IDS_URL = "https://api.stackexchange.com/2.2/posts"
+POST_IDS_NUMBER = os.environ.get('ERIDU_POST_IDS_NUMBER', 100)
+POST_IDS_FILTER = os.environ.get('ERIDU_POST_IDS_FILTER', '!3tz1WbZW5IHcz*twZ')
+POST_IDS_SORT = os.environ.get('ERIDU_POST_IDS_SORT', 'creation')
+POST_IDS_ORDER = os.environ.get('ERIDU_POST_IDS_ORDER', 'asc')
+
+QUESTIONS_URL = "https://api.stackexchange.com/2.2/questions/{}"
+QUESTIONS_FILTER = os.environ.get('ERIDU_QUESTIONS_FILTER', '!OfZM.T7F9gRuLlvhzHoyC1Fyd3oEOAMszsZJXvHk4mw')
+QUESTIONS_SORT = os.environ.get('ERIDU_QUESTIONS_SORT', 'creation')
+QUESTIONS_ORDER = os.environ.get('ERIDU_QUESTIONS_ORDER', 'asc')
+
+ANSWERS_URL = "https://api.stackexchange.com/2.2/answers/{}"
+ANSWERS_FILTER = os.environ.get('ERIDU_ANSWERS_FILTER', '!Fcazzsr2b3Mo6cWaRk)J*C-n25')
+ANSWERS_SORT = os.environ.get('ERIDU_ANSWERS_SORT', 'creation')
+ANSWERS_ORDER = os.environ.get('ERIDU_ANSWERS_ORDER', 'asc')
