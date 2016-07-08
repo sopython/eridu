3b07049651ca23b5b0157613ef73be5bf3cba69e
bluepizzey <kjpizzey@gmail.com>
2016-07-08 20:14:34 +0100

    Updated setup.py, added requirements file.

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 from setuptools import setup
 
+from pip.req import parse_requirements
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    'Click>=6.0',
-    # TODO: put package requirements here
-]
+requirements = parse_requirements('requirements.txt', session=False)
+requirements = [str(ir.req) for ir in requirements]
 
 test_requirements = [
     # TODO: put package test requirements here
@@ -29,8 +29,7 @@ setup(
     packages=[
         'eridu',
     ],
-    package_dir={'eridu':
-                 'eridu'},
+    package_dir={'eridu': 'eridu'},
     entry_points={
         'console_scripts': [
             'eridu=eridu.cli:main'
@@ -42,14 +41,14 @@ setup(
     zip_safe=False,
     keywords='eridu',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython'
     ],
     test_suite='tests',
     tests_require=test_requirements
